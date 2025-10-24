14d02fea50b87a5167d49d517090c97f40cce751
matheusplay13 <souzamatheus8972@gmail.com>
2025-10-24 10:00:07 -0300

    Primeiro commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer () {
+    return (
+        <footer className="footer">
+            <div classNAme="footer-bottom">
+                <p>Otalípio Pereira de Andrade</p>
+            </div>
+        </footer>
+    );
+};
